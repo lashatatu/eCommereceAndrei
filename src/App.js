1ed3a1c54354b3125da5866a248938a054e4b2bc
lasha tatulashvili <lashatatu@gmail.com>
2020-11-28 17:45:06 +0400

    reselect library

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import {createStructuredSelector} from "reselect";
 
 import './App.css';
-
-
 
 import HomePage from './page/homepage/homepage.component';
 import ShopPage from './page/shop/shop.component';
 import SignInAndSignUpPage from './page/sign-in-and-sign-up/sign-in-and-sign-upcomponent';
+import CheckoutPage from "./page/checkout/checkout.component";
+
 import Header from './components/header/header.component';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
+import {selectCurrentUser} from "./redux/user/user.selector";
 
 class App extends React.Component {
     unsubscribeFromAuth = null;
@@ -46,15 +49,26 @@ class App extends React.Component {
                 <Switch>
                     <Route exact path='/' component={HomePage} />
                     <Route path='/shop' component={ShopPage} />
-                    <Route exact path='/signin' render={()=>this.props.currentUser ? (<Redirect to={'/'}/>) : (<SignInAndSignUpPage />)} />
+                    <Route exact path='/checkout' component={CheckoutPage} />
+                    <Route
+                        exact
+                        path='/signin'
+                        render={()=>
+                            this.props.currentUser ? (
+                            <Redirect to={'/'}/>)
+                            : (
+                                <SignInAndSignUpPage />
+                                )
+                        }
+                    />
                 </Switch>
             </div>
         );
     }
 }
 
-const mapStateToProps=({user})=>({
-    currentUser: user.currentUser
+const mapStateToProps=createStructuredSelector({
+    currentUser: selectCurrentUser
 });
 
 const mapDispatchToProps = dispatch => ({
