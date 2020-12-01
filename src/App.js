59d35aa57a6ef978bc447070199554ef0e0b2f68
lasha tatulashvili <lashatatu@gmail.com>
2020-12-01 21:35:22 +0400

    end of section 15

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import CheckoutPage from "./page/checkout/checkout.component";
 import Header from './components/header/header.component';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
 import { setCurrentUser } from './redux/user/user.actions';
 import {selectCurrentUser} from "./redux/user/user.selector";
 
